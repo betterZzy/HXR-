8f40346a862fa6383db6a774eb0b18536c02baec
zouzhiyu <zouzhiyuxr@foxmail.com>
2023-03-26 14:55:15 +0800

    增加tkinter
    Signed-off-by: zouzhiyu <zouzhiyuxr@foxmail.com>

--- jiebao.py
+++ jiebao.py
@@ -1,4 +1,5 @@
 import os
+import tkinter
 import jieba
 import zhon.hanzi
 import collections
@@ -14,7 +15,7 @@ punc = zhon.hanzi.punctuation  #要去除的中文标点符号
 baidu_stopwords = stopwords.words(os.path.abspath('.\dict\cn_stopwords.txt')) #导入停用词表
 jieba.load_userdict('dict\教育论文分析字典.txt')    #导入自定义字典
 
-root = Tkinter.Tk()
+root = tkinter.Tk()
 root.withdraw()
 file_path = filedialog.askopenfilename()    #选择需要统计词频的文件
 with open(file_path,encoding="utf-8") as fp:
